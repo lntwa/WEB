a603213b496b6ca17c5eca25596a534b531b859d
lntwa <delonko@yandex.ru>
2025-09-19 17:56:55 +0300

    modified js file

--- lab1/canvas.js
+++ lab1/canvas.js
@@ -1,8 +1,36 @@
-function initCanvas() {
-    const canvas = document.getElementById('coordinate_plane')
-    const ctx = canvas.getContext('2d')
-    ctx.font = '13px Arial'
-    ctx.textAlign = 'center'
-    ctx.textBaseline = 'middle'
-    return ctx;
-    }
+const canvasSize = 400;
+const W = canvas.width;
+const H = canvas.height;
+const centerX = W / 2;
+const centerY = H / 2;
+
+function initCanvas(R) {
+    const canvas = document.getElementById('coordinate_plane');
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.beginPath();
+    ctx.moveTo(0, centerY);
+    ctx.lineTo(W, centerY);
+    ctx.moveTo(centerX, 0);
+    ctx.lineTo(centerX, H);
+    ctx.strokeStyle = "black"; 
+    ctx.lineWidth = 1; 
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.rect(centerX - scale * R/2, centerY - scale * R, scale * R/2, scale * R);
+    ctx.fillStyle = "rgb(26, 163, 149, 0.5)";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(centerX, centerY); 
+    ctx.lineTo(centerX + scale * R/2, centerY); 
+    ctx.lineTo(centerX, centerY - scale * R/2); 
+    ctx.closePath();
+    ctx.fillStyle = "rgb(26, 163, 149, 0.5)"
+    ctx.fill();
+
+    
+}
